f917a16965dfafb630bc1f1250fa26e9b433b579
ivanmijacika <imijacika20@stuy.edu>
2020-12-15 13:14:35 -0500

    first attempt of bubble sort without test added

--- Sorts.java
+++ Sorts.java
@@ -10,15 +10,19 @@ public class Sorts{
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-//  public static void bubbleSort(int[] data){
-//    boolean isSorted = false;
-//    while (isSorted==false){
-//      for (int i=0; i<data.length-1; i++){
-//	if (data[i]>data[i+1]){
-
-//	}
-//      }
-//    }
-//  }
+  public static void bubbleSort(int[] data){
+    boolean isSorted = false;
+    int count = 0;
+    while (isSorted==false){
+      count = 0;
+      for (int i=0; i<data.length-1; i++){
+	if (data[i]>data[i+1]){
+	  swap(data[i], data[i+1]);
+	  count++;
+	}
+      }
+      if (count == 0) isSorted=true;
+    }
+  }
 
 }
