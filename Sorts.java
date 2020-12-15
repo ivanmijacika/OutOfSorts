556c1418e1e729ce881843aaf33916e0160394a9
ivanmijacika <imijacika20@stuy.edu>
2020-12-15 13:11:15 -0500

    swap helper added

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,24 @@
 public class Sorts{
-  /**Bubble sort of an int array. 
+
+  public static void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+
+  /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+//  public static void bubbleSort(int[] data){
+//    boolean isSorted = false;
+//    while (isSorted==false){
+//      for (int i=0; i<data.length-1; i++){
+//	if (data[i]>data[i+1]){
+
+//	}
+//      }
+//    }
+//  }
+
 }
